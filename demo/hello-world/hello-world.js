f9e3ed3f540295311845adb74d9436c0e6edc937
Juan Pablo de Lima B <47327855+delima02@users.noreply.github.com>
2019-02-27 13:33:10 -0500

    Remove changes to hello-world demo.

--- demo/hello-world/hello-world.js
+++ demo/hello-world/hello-world.js
@@ -20,7 +20,7 @@ btn.addEventListener('click', async () => {
   const h1 = document.createElement('h1');
   h1.textContent = 'Hello World!'
   document.body.appendChild(h1);
+  
   const boundingClientRect = await h1.getBoundingClientRectAsync();
   h1.textContent = h1.textContent + JSON.stringify(boundingClientRect);
-  
 });
