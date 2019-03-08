ampproject/worker-dom

commit 22a590ce896912400932c05a17df261886ee894e

tagName should be uppercase (remix) (#329)
* Roll forward uppercase tagName.

* Use localName in createElementNS(), lowercase in createElement().

* Update createElement tests.

* Lowercase registerSubclass() calls.

* Fix registerSubclass() for non-HTML namespaces.

* Bump bundlesize cap for worker.mjs.

* Simplify test slightly.

* Remove registerSubclass test (covered by createElement).

* Add test for namespaceURI.

src/main-thread/nodes.ts
@@ -67,8 +67,8 @@ export class NodeContext {
     } else {
       const namespace =
         skeleton[TransferrableKeys.namespaceURI] !== undefined ? this.strings.get(skeleton[TransferrableKeys.namespaceURI] as number) : undefined;
-      const nodeName = this.strings.get(skeleton[TransferrableKeys.nodeName]);
-      node = namespace ? document.createElementNS(namespace, nodeName) : document.createElement(nodeName);
+      const localName = this.strings.get(skeleton[TransferrableKeys.localOrNodeName]);
+      node = namespace ? document.createElementNS(namespace, localName) : document.createElement(localName);
 
       // TODO(KB): Restore Properties
       // skeleton.properties.forEach(property => {

src/worker-thread/dom/Element.ts
@@ -20,7 +20,7 @@ import { DOMTokenList } from './DOMTokenList';
 import { Attr, toString as attrsToString, matchPredicate as matchAttrPredicate } from './Attr';
 import { mutate } from '../MutationObserver';
 import { MutationRecordType } from '../MutationRecord';
-import { NumericBoolean } from '../../utils';
+import { NumericBoolean, toLower, toUpper } from '../../utils';
 import { Text } from './Text';
 import { CSSStyleDeclaration } from '../css/CSSStyleDeclaration';
 import { matchChildrenElements } from './matchElements';
@@ -28,13 +28,13 @@ import { reflectProperties } from './enhanceElement';
 import { TransferrableKeys } from '../../transfer/TransferrableKeys';
 import { HydrateableNode, NodeType, HTML_NAMESPACE } from '../../transfer/TransferrableNodes';
 import { store as storeString } from '../strings';
-import { toLower } from '../../utils';
 import { MessageToWorker, MessageType, BoundingClientRectToWorker } from '../../transfer/Messages';
 import { TransferrableBoundingClientRect } from '../../transfer/TransferrableCommands';
 
-export const NODE_NAME_MAPPING: { [key: string]: typeof Element } = {};
-export function registerSubclass(nodeName: NodeName, subclass: typeof Element): void {
-  NODE_NAME_MAPPING[nodeName] = subclass;
+export const NS_NAME_TO_CLASS: { [key: string]: typeof Element } = {};
+export function registerSubclass(localName: string, subclass: typeof Element, namespace: string = HTML_NAMESPACE): void {
+  const key = `${namespace}:${localName}`;
+  NS_NAME_TO_CLASS[key] = subclass;
 }
 
 interface ClientRect {
@@ -56,15 +56,15 @@ export class Element extends ParentNode {
   public style: CSSStyleDeclaration = new CSSStyleDeclaration(this);
   public namespaceURI: NamespaceURI;
 
-  constructor(nodeType: NodeType, nodeName: NodeName, namespaceURI: NamespaceURI, ownerDocument: Node | null) {
-    super(nodeType, nodeName, ownerDocument);
+  constructor(nodeType: NodeType, localName: NodeName, namespaceURI: NamespaceURI, ownerDocument: Node | null) {
+    super(nodeType, toUpper(localName), ownerDocument);
     this.namespaceURI = namespaceURI || HTML_NAMESPACE;
-    this.localName = toLower(nodeName);
+    this.localName = localName;
     this[TransferrableKeys.creationFormat] = {
       [TransferrableKeys.index]: this[TransferrableKeys.index],
       [TransferrableKeys.transferred]: NumericBoolean.FALSE,
       [TransferrableKeys.nodeType]: this.nodeType,
-      [TransferrableKeys.nodeName]: storeString(this.nodeName),
+      [TransferrableKeys.localOrNodeName]: storeString(this.localName),
       [TransferrableKeys.namespaceURI]: this.namespaceURI === null ? undefined : storeString(this.namespaceURI),
     };
   }

src/main-thread/debugging.ts
@@ -173,7 +173,7 @@ export class DebuggingContext {
 function readableHydrateableNode(node: HydrateableNode): Object {
   const out: any = {
     nodeType: node[TransferrableKeys.nodeType],
-    nodeName: node[TransferrableKeys.nodeName],
+    name: node[TransferrableKeys.localOrNodeName],
   };
   const attributes = node[TransferrableKeys.attributes];
   if (attributes) {

src/main-thread/serialize.ts
@@ -30,7 +30,7 @@ function createHydrateableNode(element: RenderableElement, minimizeString: (valu
     [TransferrableKeys.index]: element._index_,
     [TransferrableKeys.transferred]: NumericBoolean.FALSE,
     [TransferrableKeys.nodeType]: element.nodeType,
-    [TransferrableKeys.nodeName]: minimizeString(element.nodeName),
+    [TransferrableKeys.localOrNodeName]: minimizeString(element.localName || element.nodeName),
     [TransferrableKeys.childNodes]: [].map.call(element.childNodes || [], (child: RenderableElement) => createHydrateableNode(child, minimizeString)),
     [TransferrableKeys.attributes]: [].map.call(element.attributes || [], (attribute: Attr) => [
       minimizeString(attribute.namespaceURI || 'null'),

src/test/document/createElement.ts
@@ -1,0 +1,77 @@
+/**
+ * Copyright 2019 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import anyTest, { TestInterface } from 'ava';
+import { Document, createDocument } from '../../worker-thread/dom/Document';
+import { SVGElement } from '../../worker-thread/dom/SVGElement';
+
+const test = anyTest as TestInterface<{
+  document: Document;
+}>;
+
+test.beforeEach(t => {
+  const document = createDocument();
+  t.context = { document };
+});
+
+test('createElement(x) should lowercase x', t => {
+  const { document } = t.context;
+
+  let el = document.createElement('div');
+  t.is(el.nodeName, 'DIV');
+  t.is(el.localName, 'div');
+
+  el = document.createElement('DIV');
+  t.is(el.nodeName, 'DIV');
+  t.is(el.localName, 'div');
+
+  el = document.createElement('SvG');
+  t.is(el.nodeName, 'SVG');
+  t.is(el.localName, 'svg');
+  t.false(el instanceof SVGElement);
+});
+
+test('createElement() should use HTML namespace', t => {
+  const { document } = t.context;
+
+  let el = document.createElement('div');
+  t.is(el.namespaceURI, 'http://www.w3.org/1999/xhtml');
+
+  el = document.createElement('svg');
+  t.is(el.namespaceURI, 'http://www.w3.org/1999/xhtml');
+});
+
+test('createElementNS(x) should not lowercase x', t => {
+  const { document } = t.context;
+
+  let el = document.createElementNS('http://www.w3.org/1999/xhtml', 'div');
+  t.is(el.nodeName, 'DIV');
+  t.is(el.localName, 'div');
+
+  el = document.createElementNS('http://www.w3.org/1999/xhtml', 'DIV');
+  t.is(el.nodeName, 'DIV');
+  t.is(el.localName, 'DIV');
+
+  el = document.createElementNS('http://www.w3.org/2000/svg', 'SvG');
+  t.is(el.nodeName, 'SVG');
+  t.is(el.localName, 'SvG');
+  t.false(el instanceof SVGElement);
+
+  el = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  t.is(el.nodeName, 'svg');
+  t.is(el.localName, 'svg');
+  t.true(el instanceof SVGElement);
+});

src/test/htmlelement/serialize.ts
@@ -47,7 +47,7 @@ test.beforeEach(t => {
 test('Element should serialize to a TransferrableNode', t => {
   const serializedDiv = t.context.div.hydrate();
   t.is(serializedDiv[TransferrableKeys.nodeType], NodeType.ELEMENT_NODE);
-  t.is(serializedDiv[TransferrableKeys.nodeName], get('div') as number);
+  t.is(serializedDiv[TransferrableKeys.localOrNodeName], get('div') as number);
 
   t.not(serializedDiv[TransferrableKeys.childNodes], undefined);
   t.is((serializedDiv[TransferrableKeys.childNodes] as Array<HydrateableNode>).length, 1);

src/test/node/nodeName.ts
@@ -24,5 +24,5 @@ test('returns the name of the Node', t => {
 
   t.is(document.nodeName, '#document', 'document node returns a valid document node name');
   t.is(node.nodeName, '#text', 'text node returns a valid text node name');
-  t.is(nodeTwo.nodeName, 'div', 'standard element node returns a valid node name');
+  t.is(nodeTwo.nodeName, 'DIV', 'standard element node returns a valid node name');
 });

src/transfer/TransferrableKeys.ts
@@ -16,7 +16,7 @@
 
 export const enum TransferrableKeys {
   nodeType = 0,
-  nodeName = 1,
+  localOrNodeName = 1,
   attributes = 2,
   properties = 3,
   childNodes = 4,

commit 5e36dd7df929e28fa1eef5589f00d8073ce7202a

Implement set innerHTML. (#308)
* Draft PR

* Draft PR update.

* Remove third_party folder and its contents from top level.

* Only keep necessary configs and remove example used for testing.

* Fix tests.

* Add license file for html-parser.

* Consider Comment nodes in nodes.addNode().

* Add HTML parser.

* Implement set innerHTML.

* Some formatting and error considerations.

* Use the appropriate string retrieval method.

* Add attributes to child nodes.

* Update src/third_party/html-parser/LICENSE

Co-Authored-By: delima02 <47327855+delima02@users.noreply.github.com>

* Fix LICENSE.

* Refine rollup config file.

* NIT

* Remove previous children when setting innerHTML.

* Add a test and make existing ones more specific.

* Use non-null assertions instead of conditional blocks.

* Add README file.

* Remove changes to hello-world demo.

* NIT

* Refactor removal of previous child nodes for set innerHTML.

* Removed unused/unnecessary code.

* Refine 'kMarkupPattern' RegEx

* Store ownerDocument in a variable.

* Minor fixes.

* Address some comments.

* Make the response a record.

* Add error message missed in previous commits.

* Fixed attribute matching RegEx.

* Fix parser return types.

* Do attribute parsing and setting in a single loop.

* Move 'invalid' error to the parser.

* Remove unnecessary | operator from attribute RegEx.

* Remove unnecessary code.

* Make kElementsClosedByOpening use upper case.

* Fix case in kElementsClosedByOpening & don't return valid flag

* Add a test for elements wirth attributes.

* Remove unnecessary interface and record for attributes.

* Fix case on block text elements.

* Add a different capture group for comment contents.

* Remove unnecessary code.

* Store block text element contents.

* Add variable names to results from regex array.

* Fix case in kSelfClosingElements and add necessary condition.

* Add two more unit tests.

* Refactor.

* Fix issue with tag names and add a test.

* Add some tests for the future.

* Increase max bundle size.

* NIT - addressing comments.

* Stop examining string contents.

* Add two more tests and fix invalid properties.

* Simplify block text element logic and add test

* Fix tests that broke with #329

src/main-thread/nodes.ts
@@ -64,6 +64,8 @@ export class NodeContext {
       node = document.createTextNode(this.strings.get(skeleton[TransferrableKeys.textContent] as number));
     } else if (skeleton[TransferrableKeys.nodeType] === NodeType.DOCUMENT_FRAGMENT_NODE) {
       node = document.createDocumentFragment();
+    } else if (skeleton[TransferrableKeys.nodeType] === NodeType.COMMENT_NODE) {
+      node = document.createComment(this.strings.get(skeleton[TransferrableKeys.textContent] as number));
     } else {
       const namespace =
         skeleton[TransferrableKeys.namespaceURI] !== undefined ? this.strings.get(skeleton[TransferrableKeys.namespaceURI] as number) : undefined;

src/third_party/html-parser/LICENSE
@@ -1,0 +1,19 @@
+Copyright 2019 Xiaoyi Shi <ashi009@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO
+EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.

src/third_party/html-parser/html-parser.ts
@@ -1,0 +1,205 @@
+import { Comment } from '../../worker-thread/dom/Comment';
+import { Element } from '../../worker-thread/dom/Element';
+import { Node } from '../../worker-thread/dom/Node';
+import { Text } from '../../worker-thread/dom/Text';
+
+interface Elements {
+  [key: string]: boolean;
+}
+
+interface ElementMapping {
+  [key: string]: Elements;
+}
+
+function arr_back<T>(arr: T[]) {
+  return arr[arr.length - 1];
+}
+
+// https://html.spec.whatwg.org/multipage/custom-elements.html#valid-custom-element-name
+const kMarkupPattern = /<!--([^]*)-->|<(\/?)([a-z][-.0-9_a-z]*)([^>]*?)(\/?)>/gi;
+// https://html.spec.whatwg.org/multipage/syntax.html#attributes-2
+const kAttributePattern = /(^|\s)([^\s"'>\/=]+)\s*=\s*("([^"]+)"|'([^']+)'|(\S+))/gi;
+
+const kSelfClosingElements: Elements = {
+  AREA: true,
+  BASE: true,
+  BR: true,
+  COL: true,
+  HR: true,
+  IMG: true,
+  INPUT: true,
+  LINK: true,
+  META: true,
+  PARAM: true,
+  SOURCE: true,
+  TRACK: true,
+  WBR: true
+};
+const kElementsClosedByOpening: ElementMapping = {
+  LI: { LI: true },
+  DT: { DT: true, DD: true },
+  DD: { DD: true, DT: true },
+  P: { ADDRESS: true, ARTICLE: true, ASIDE: true, BLOCKQUOTE: true, 
+    DETAILS: true, DIV: true, DL: true, FIELDSET: true, FIGCAPTION: true, 
+    FIGURE: true, FOOTER: true, FORM: true, H1: true, H2: true, H3: true, 
+    H4: true, H5: true, H6: true, HEADER: true, HR: true, MAIN: true, 
+    NAV: true, OL: true, P: true, PRE: true, SECTION: true, TABLE: true,
+    UL: true },
+  RT: { RT: true, RP: true },
+  RP: { RT: true, RP: true },
+  OPTGROUP: { OPTGROUP: true },
+  OPTION: { OPTION: true, OPTGROUP: true },
+  THEAD: { TBODY: true, TFOOT: true },
+  TBODY: { TBODY: true, TFOOT: true },
+  TR: { TR: true },
+  TD: { TD: true, TH: true },
+  TH: { TD: true, TH: true },
+};
+const kElementsClosedByClosing: ElementMapping = {
+  LI: { UL: true, OL: true },
+  A: { DIV: true },
+  B: { DIV: true },
+  I: { DIV: true },
+  P: { DIV: true },
+  TD: { TR: true, TABLE: true },
+  TH: { TR: true, TABLE: true },
+};
+const kBlockTextElements: Elements = {
+  SCRIPT: true,
+  NOSCRIPT: true,
+  STYLE: true,
+  PRE: true,
+};
+
+/**
+ * Parses HTML and returns a root element
+ * Parse a chuck of HTML source.
+ * @param  {string} data HTML in string format.
+ * @param {!Element} root The element to use as root.
+ * @return {Element}      root element
+ */
+export function parse(data: string, rootElement: Element) {
+  const ownerDocument = rootElement.ownerDocument;
+  const root = new Element(rootElement.nodeType, rootElement.nodeName, rootElement.namespaceURI, ownerDocument);
+
+  let currentParent = root as Node;
+  const stack = [root as Node];
+  let lastTextPos = 0;
+  let match: RegExpExecArray | null;
+  data = '<div>' + data + '</div>';
+  const tagsClosed = [] as string[];
+
+  while ((match = kMarkupPattern.exec(data))) {
+
+    const commentContents = match[1]; // <!--contents-->
+    let beginningSlash = match[2]; // ... </ ...
+    const tagName = match[3];
+    const matchAttributes = match[4];
+    const endSlash = match[5]; // ... /> ...
+
+    if (lastTextPos < match.index) {
+      // if has content
+      const text = data.slice(lastTextPos, match.index);
+      currentParent.appendChild(new Text(text, ownerDocument));
+    }
+    lastTextPos = kMarkupPattern.lastIndex;
+    if (commentContents !== undefined) {
+      // this is a comment
+      currentParent.appendChild(new Comment(commentContents, ownerDocument));
+      continue;
+    }
+
+    const normalizedTagName = tagName.toUpperCase();
+
+    if (!beginningSlash) {
+      // not </ tags
+      if (!endSlash && kElementsClosedByOpening[currentParent.tagName]) {
+        if (kElementsClosedByOpening[currentParent.tagName][normalizedTagName]) {
+          tagsClosed.push(currentParent.tagName);
+        }
+      }
+      const childToAppend = new Element(
+        currentParent.nodeType, 
+        tagName.toLowerCase(), // TODO only do this for HTML namespace elements
+        currentParent.namespaceURI, 
+        ownerDocument);
+
+      for (let attMatch; (attMatch = kAttributePattern.exec(matchAttributes)); ) {
+        const attrName = attMatch[2];
+        const attrValue = attMatch[4] || attMatch[5] || attMatch[6];
+        childToAppend.setAttribute(attrName, attrValue);
+      }
+
+      currentParent = currentParent.appendChild(childToAppend);
+
+      stack.push(currentParent);
+      if (kBlockTextElements[normalizedTagName]) {
+        // a little test to find next </script> or </style> ...
+        const closeMarkup = '</' + normalizedTagName.toLowerCase() + '>';
+        const index = data.indexOf(closeMarkup, kMarkupPattern.lastIndex);
+        if (index == -1) {
+          throw new Error('Close markup not found.');
+        } else {
+          kMarkupPattern.lastIndex = index;
+        }
+      }
+    }
+    if (beginningSlash || endSlash || kSelfClosingElements[normalizedTagName]) {
+      // </ or /> or <br> etc.
+      while (true) {
+        if (stack.length <= 1) {
+          break;
+        }
+
+        if (currentParent.nodeName.toUpperCase() == normalizedTagName) {
+          stack.pop();
+          currentParent = arr_back(stack);
+          break;
+        } else {
+          // Trying to close current tag, and move on
+          if (kElementsClosedByClosing[currentParent.tagName]) {
+            if (kElementsClosedByClosing[currentParent.tagName][normalizedTagName]) {
+              stack.pop();
+              currentParent = arr_back(stack);
+              continue;
+            }
+          }
+          // Use aggressive strategy to handle unmatching markups.
+          break;
+        }
+      }
+    }
+  }
+
+  for (const node of stack) {
+    if (tagsClosed[tagsClosed.length - 1] == node.nodeName) {
+      stack.pop();
+      tagsClosed.pop();
+      currentParent = arr_back(stack);
+    } else break;
+  }
+
+  const valid = stack.length === 1;
+
+  if (!valid) {
+    throw new Error('Attempting to parse invalid HTML content.');
+  }
+
+  root.childNodes.forEach(node => {
+    if (node instanceof Element) {
+      node.parentNode = null;
+    }
+  });
+
+  // remove the added <div>
+  if (root.firstChild) {
+    root.firstChild.childNodes.forEach(node => {
+      if (node instanceof Node) {
+        node.parentNode = null;
+      }
+    });
+    return root.firstChild;
+  }
+
+  throw new Error('Attempting to parse invalid HTML.');
+}

src/worker-thread/dom/Element.ts
@@ -30,6 +30,8 @@ import { HydrateableNode, NodeType, HTML_NAMESPACE } from '../../transfer/Transf
 import { store as storeString } from '../strings';
 import { MessageToWorker, MessageType, BoundingClientRectToWorker } from '../../transfer/Messages';
 import { TransferrableBoundingClientRect } from '../../transfer/TransferrableCommands';
+import { parse } from '../../third_party/html-parser/html-parser';
+import { propagate } from './Node';
 
 export const NS_NAME_TO_CLASS: { [key: string]: typeof Element } = {};
 export function registerSubclass(localName: string, subclass: typeof Element, namespace: string = HTML_NAMESPACE): void {
@@ -167,6 +169,33 @@ export class Element extends ParentNode {
         .join('');
     }
     return '';
+  }
+
+  /**
+   * @see https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML
+   * @param html The raw html string to parse.
+   */
+  set innerHTML(html: string) {
+    const root = parse(html, this);
+
+    // remove previous children
+    this.childNodes.forEach(n => {
+      propagate(n, 'isConnected', false);
+      propagate(n, TransferrableKeys.scopingRoot, n);
+    });
+
+    mutate({
+      removedNodes: this.childNodes,
+      type: MutationRecordType.CHILD_LIST,
+      target: this,
+    });
+
+    this.childNodes = [];
+
+    // add new children
+    root.childNodes.forEach(n => {
+      this.appendChild(n);
+    });
   }
 
   /**

src/test/element/innerHTML.ts
@@ -18,6 +18,7 @@ import anyTest, { TestInterface } from 'ava';
 import { Element } from '../../worker-thread/dom/Element';
 import { Text } from '../../worker-thread/dom/Text';
 import { Comment } from '../../worker-thread/dom/Comment';
+import { NodeType, SVG_NAMESPACE } from '../../transfer/TransferrableNodes';
 import { createDocument } from '../../worker-thread/dom/Document';
 
 const test = anyTest as TestInterface<{
@@ -65,4 +66,131 @@ test('element with comment', t => {
 
   node.appendChild(comment);
   t.is(node.innerHTML, '<!--comment-->');
+});
+
+test('set nothing', t => {
+  const { node } = t.context;
+  node.innerHTML = '';
+  t.is(node.hasChildNodes(), false);
+});
+
+test('set replaces children', t => {
+  const { node, child } = t.context;
+  node.appendChild(child);
+  node.innerHTML = '';
+  t.is(node.hasChildNodes(), false);
+});
+
+test('set an element node', t => {
+  const { node } = t.context;
+  node.innerHTML = '<div></div>';
+  t.is(node.childNodes.length, 1);
+
+  const child = node.firstChild!;
+  t.is(child.nodeType, NodeType.ELEMENT_NODE);
+  t.is(child.nodeName, 'DIV');
+});
+
+test('set a text node', t => {
+  const { node } = t.context;
+  const testString = 'Hello, World!';
+  node.innerHTML = testString;
+  t.is(node.childNodes.length, 1);
+
+  const child = node.firstChild!;
+  t.is(child.nodeType, NodeType.TEXT_NODE);
+  t.is(child.textContent, testString);
+});
+
+test('set comment node', t => {
+  const { node } = t.context;
+  const testString = 'this is a comment';
+  node.innerHTML = '<!--' + testString + '-->';
+  t.is(node.childNodes.length, 1);
+
+  const child = node.firstChild!;
+  t.is(child.nodeType, NodeType.COMMENT_NODE);
+  t.is(child.textContent, testString);
+});
+
+test('set nested elements', t => {
+  const { node } = t.context;
+  node.innerHTML = '<div><div></div></div>';
+  t.is(node.childNodes.length, 1);
+
+  const child = node.firstChild!;
+  t.is(child.childNodes.length, 1);
+});
+
+test('set non-nested elements', t => {
+  const { node } = t.context;
+  node.innerHTML = '<div></div><div></div>';
+  t.is(node.childNodes.length, 2);
+});
+
+test('set element with attributes', t => {
+  const { node } = t.context;
+  node.innerHTML = '<div hi="hello"></div>';
+  const child = node.firstChild!;
+  t.is(child.attributes.length, 1);
+  t.is(child.attributes[0].name, "hi");
+  t.is(child.attributes[0].value, "hello");
+});
+
+test('set self closing tags', t => {
+  const { node } = t.context;
+  node.innerHTML = '<br>';
+  const child = node.firstChild!;
+  t.is(child.nodeType, NodeType.ELEMENT_NODE);
+  t.is(child.nodeName, 'BR');
+});
+
+test('set invalid html throws', t => {
+  const { node } = t.context;
+  // Use an unclosed tag.
+  t.throws(() => node.innerHTML = '<div>');
+});
+
+test('set closes tags by closing others', t => {
+  const { node } = t.context;
+  node.innerHTML = '<div><a></div>';
+  let child = node.firstChild!;
+  t.is(child.nodeName, 'DIV');
+  t.is(child.firstChild!.nodeName, 'A');
+
+  node.innerHTML = '<a><div></div>';
+  child = node.firstChild!;
+  t.is(child.nodeName, 'A');
+  t.is(child.firstChild!.nodeName, 'DIV');
+});
+
+test('set takes all block text element content as text', t => {
+  const { node } = t.context;
+  node.innerHTML = '<style><div></div></style>';
+  const child = node.firstChild!; // style node
+  const childContent = child.firstChild!;
+  t.is(childContent.nodeType, NodeType.TEXT_NODE);
+});
+
+test('set normalizes html namespace tag names', t => {
+  const { node } = t.context;
+  node.innerHTML = '<Div></Div>';
+  const child = node.firstChild!;
+  t.is(child.localName, 'div');
+  t.is(child.nodeName, 'DIV');
+});
+
+test.skip('set has svg tags live in SVG namespace', t => {
+  const { node } = t.context;
+  node.innerHTML = '<svg></svg>';
+  const child = node.firstChild!;
+  t.is(child.namespaceURI, SVG_NAMESPACE);
+});
+
+test.skip('set keeps tagName\'s case', t => {
+  const { node } = t.context;
+  node.innerHTML = '<svg><feImage></feImage></svg>';
+  const svgWrapper = node.firstChild!;
+  const child = svgWrapper.firstChild!;
+  t.is(child.nodeName, 'feImage');
 });