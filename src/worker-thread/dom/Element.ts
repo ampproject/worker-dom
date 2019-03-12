01a74d7ee3a8760a784b0ac782b44c93061114ae
William Chou <willchou@google.com>
2019-03-12 11:14:42 -0400

    Avoid super.getter. (#332)

--- src/worker-thread/dom/Element.ts
+++ src/worker-thread/dom/Element.ts
@@ -254,7 +254,7 @@ export class Element extends ParentNode {
    * @return text from all childNodes.
    */
   get textContent(): string {
-    return super.textContent;
+    return this.getTextContent();
   }
 
   /**
