401381208af020f19ecab06722ba8fa865b8c261
delima02 <jpdelima@google.com>
2019-04-02 15:42:51 -0400

    Fix indentation in offscreen-canvas.ts

--- src/main-thread/commands/offscreen-canvas.ts
+++ src/main-thread/commands/offscreen-canvas.ts
@@ -5,26 +5,25 @@ import { CommandExecutor } from './interface';
 import { OffscreenCanvasMutationIndex } from '../../transfer/TransferrableMutation';
 
 export function OffscreenCanvasProcessor(workerContext: WorkerContext): CommandExecutor {
-    return {
-        execute(mutations: Uint16Array, startPosition: number, target: RenderableElement): number {
-            if (target) {
-                const offscreen = (target as HTMLCanvasElement).transferControlToOffscreen();
-                workerContext.messageToWorker({
-                    [TransferrableKeys.type]: MessageType.OFFSCREEN_CANVAS_INSTANCE,
-                    [TransferrableKeys.target]: [target._index_],
-                    [TransferrableKeys.data]: offscreen, // Object, an OffscreenCanvas
-                }, [offscreen]);
-            } else {
-                console.error(`getNode() yields null – ${target}`);
-            }
-
-            return startPosition + OffscreenCanvasMutationIndex.End;
-        },
-        print(mutations: Uint16Array, startPosition: number, target?: RenderableElement | null): Object {
-            return {
-              type: 'OFFSCREEN_CANVAS_INSTANCE',
-              target,
-            };
-          },
-    };
+  return {
+    execute(mutations: Uint16Array, startPosition: number, target: RenderableElement): number {
+      if (target) {
+        const offscreen = (target as HTMLCanvasElement).transferControlToOffscreen();
+        workerContext.messageToWorker({
+            [TransferrableKeys.type]: MessageType.OFFSCREEN_CANVAS_INSTANCE,
+            [TransferrableKeys.target]: [target._index_],
+            [TransferrableKeys.data]: offscreen, // Object, an OffscreenCanvas
+        }, [offscreen]);
+      } else {
+        console.error(`getNode() yields null – ${target}`);
+      }
+    
+      return startPosition + OffscreenCanvasMutationIndex.End;
+    }, print(mutations: Uint16Array, startPosition: number, target?: RenderableElement | null): Object {
+      return {
+          type: 'OFFSCREEN_CANVAS_INSTANCE',
+          target,
+      };
+    },
+  };
 }
