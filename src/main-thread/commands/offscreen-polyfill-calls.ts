7008704f2f8a96ea02126a353633911da860a0cb
delima02 <jpdelima@google.com>
2019-04-13 14:18:13 -0400

    Fix polyfill CommandExectutor's print() impl.

--- src/main-thread/commands/offscreen-polyfill-calls.ts
+++ src/main-thread/commands/offscreen-polyfill-calls.ts
@@ -54,29 +54,49 @@ export function OffscreenPolyfillCallProcessor(strings: Strings, workerContext: 
     print(mutations: Uint16Array, startPosition: number, target?: RenderableElement | null): Object {
       const float32Needed = mutations[startPosition + OffscreenContextPolyfillMutationIndex.Float32Needed] === NumericBoolean.TRUE;
       const argCount = mutations[startPosition + OffscreenContextPolyfillMutationIndex.ArgumentCount];
-      let mutationsArray: Uint16Array | Float32Array;
-      let endOffset = OffscreenContextPolyfillMutationIndex.End + argCount;
+      const methodCalled = strings.get(mutations[startPosition + OffscreenContextPolyfillMutationIndex.MethodCalled]);
+      const isSetter = mutations[startPosition + OffscreenContextPolyfillMutationIndex.IsSetter] === NumericBoolean.TRUE;
+      const stringArgIndex = mutations[startPosition + OffscreenContextPolyfillMutationIndex.StringArgIndex];
+
+      const argsStart = startPosition + OffscreenContextPolyfillMutationIndex.Args;
+      let argsTypedArray: Uint16Array | Float32Array;
+      let argEnd = argCount;
 
       if (float32Needed) {
-        endOffset = endOffset * 2;
-        mutationsArray = new Float32Array(mutations.slice(startPosition, startPosition + endOffset).buffer);
+        argEnd *= 2;
+        argsTypedArray = new Float32Array(mutations.slice(argsStart, argsStart + argEnd).buffer);
       } else {
-        mutationsArray = mutations.slice(startPosition, startPosition + endOffset);
+        argsTypedArray = mutations.slice(argsStart, argsStart + argEnd);
       }
 
-      const methodCalled = strings.get(mutationsArray[OffscreenContextPolyfillMutationIndex.MethodCalled]);
-      const isSetter = mutationsArray[OffscreenContextPolyfillMutationIndex.IsSetter] === NumericBoolean.TRUE;
-      const stringArgIndex = mutationsArray[OffscreenContextPolyfillMutationIndex.StringArgIndex];
+      let args = [] as any[];
+
+      if (argCount > 0) {
+        argsTypedArray.forEach((arg: any, i: number) => {
+          if (stringArgIndex - 1 === i) {
+            args.push(strings.get(arg));
+          } else {
+            args.push(arg);
+          }
+        });
+
+        // setLineDash has a single argument: number[]
+        // values from the array argument are transferred independently, so we must do this
+        if (methodCalled === 'setLineDash') {
+          args = [args];
+        }
+      }
 
       return {
         type: 'OFFSCREEN_POLYFILL',
         target,
-        IsFloat32ArrayNeeded: float32Needed,
-        MethodName: methodCalled,
+        Float32Needed: float32Needed,
+        ArgumentCount: argCount,
+        MethodCalled: methodCalled,
         IsSetter: isSetter,
         StringArgIndex: stringArgIndex,
-        ArgCount: argCount,
-        End: endOffset,
+        Args: args,
+        End: startPosition + OffscreenContextPolyfillMutationIndex.End + argEnd,
       };
     },
   };
