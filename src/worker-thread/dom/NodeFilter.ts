0abf98638ee2accf0a9350403b103372f3054eee
Jeremy Danyow <jdanyow@gmail.com>
2019-08-25 17:55:55 -0700

    remove deprecated constants

--- src/worker-thread/dom/NodeFilter.ts
+++ src/worker-thread/dom/NodeFilter.ts
@@ -7,17 +7,12 @@ export abstract class NodeFilter {
 
   public static readonly SHOW_ALL = 4294967295;
   public static readonly SHOW_ELEMENT = 1;
-  public static readonly SHOW_ATTRIBUTE = 2;
   public static readonly SHOW_TEXT = 4;
-  public static readonly SHOW_CDATA_SECTION = 8;
-  public static readonly SHOW_ENTITY_REFERENCE = 16;
-  public static readonly SHOW_ENTITY = 32;
   public static readonly SHOW_PROCESSING_INSTRUCTION = 64;
   public static readonly SHOW_COMMENT = 128;
   public static readonly SHOW_DOCUMENT = 256;
   public static readonly SHOW_DOCUMENT_TYPE = 512;
   public static readonly SHOW_DOCUMENT_FRAGMENT = 1024;
-  public static readonly SHOW_NOTATION = 2048;
 }
 
 export interface NodeFilter {
