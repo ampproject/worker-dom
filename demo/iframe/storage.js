8e4948794b65a1e6149aec764d67fa6a96620036
Jake Fried <samouri@users.noreply.github.com>
2020-11-18 12:55:37 -0500

    simulate incognito crossorigin

--- demo/iframe/storage.js
+++ demo/iframe/storage.js
@@ -1,12 +1,14 @@
 try {
   localStorage.setItem('marco', 'polo');
-  console.log('localStorage is available');
+  console.log('localStorage is available at: ' + window.location);
 } catch (e) {
   console.error(e);
 }
 try {
   sessionStorage.setItem('marco', 'polo');
-  console.log('sessionStorage is available');
+  console.log('sessionStorage is available at: ' + window.location);
 } catch (e) {
   console.error(e);
 }
+
+console.log('Can run code after the storage access occurs');
