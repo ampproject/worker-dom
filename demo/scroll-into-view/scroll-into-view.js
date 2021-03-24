831dffc2e422965e3857391f8476d988325ecd10
Andrei O <andrei@flashsoft.eu>
2021-03-24 20:55:33 +0200

    Update demo/scroll-into-view/scroll-into-view.js
    Co-authored-by: Jake Fried <samouri@users.noreply.github.com>

--- demo/scroll-into-view/scroll-into-view.js
+++ demo/scroll-into-view/scroll-into-view.js
@@ -1,6 +1,6 @@
 document.getElementById('scroll-button').addEventListener('click', () => {
   document.getElementById('scroll-element').scrollIntoView();
-  });
+});
 
 document.getElementById('scroll-button-up').addEventListener('click', () => {
   document.getElementById('scroll-button').scrollIntoView();
