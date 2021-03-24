a9015ee1829dd54de09cfb8bb832e02c2ad856f8
Andrei O. (andrei0x309) <andrei@flashsoft.eu>
2021-03-24 21:22:35 +0200

    Ran prettier on file

--- src/test/mutation-transfer/scrollIntoView.test.ts
+++ src/test/mutation-transfer/scrollIntoView.test.ts
@@ -14,38 +14,40 @@
  * limitations under the License.
  */
 
- import anyTest, { TestInterface } from 'ava';
- import { Document } from '../../worker-thread/dom/Document';
- import { TransferrableMutationType } from '../../transfer/TransferrableMutation';
- import { createTestingDocument } from '../DocumentCreation';
- import { expectMutations } from '../Emitter';
- import { Element } from '../../worker-thread/dom/Element';
- import { TransferrableKeys } from '../../transfer/TransferrableKeys';
- 
- const test = anyTest as TestInterface<{
-   document: Document;
-   element: Element;
- }>;
- 
- test.beforeEach((t) => {
-   const document = createTestingDocument();
-   const element = document.createElement('div');
- 
-   t.context = {
-     document,
-     element,
-   };
- });
- 
- test.serial.cb('Element.scrollIntoView() transfer to main-thread', (t) => {
-   const { document, element } = t.context;
-   element.isConnected = true;
+import anyTest, { TestInterface } from "ava";
+import { Document } from "../../worker-thread/dom/Document";
+import { TransferrableMutationType } from "../../transfer/TransferrableMutation";
+import { createTestingDocument } from "../DocumentCreation";
+import { expectMutations } from "../Emitter";
+import { Element } from "../../worker-thread/dom/Element";
+import { TransferrableKeys } from "../../transfer/TransferrableKeys";
 
-   expectMutations(document, (mutations) => {
-     t.deepEqual(mutations, [TransferrableMutationType.SCROLL_INTO_VIEW, element[TransferrableKeys.index]]);
-     t.end();
-   });
- 
-   element.scrollIntoView();
- });
- 
+const test = anyTest as TestInterface<{
+  document: Document;
+  element: Element;
+}>;
+
+test.beforeEach((t) => {
+  const document = createTestingDocument();
+  const element = document.createElement("div");
+
+  t.context = {
+    document,
+    element,
+  };
+});
+
+test.serial.cb("Element.scrollIntoView() transfer to main-thread", (t) => {
+  const { document, element } = t.context;
+  element.isConnected = true;
+
+  expectMutations(document, (mutations) => {
+    t.deepEqual(mutations, [
+      TransferrableMutationType.SCROLL_INTO_VIEW,
+      element[TransferrableKeys.index],
+    ]);
+    t.end();
+  });
+
+  element.scrollIntoView();
+});
