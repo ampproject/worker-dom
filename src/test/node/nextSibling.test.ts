c906bfc526bc1f6a43bc309228ac04dd99151ba2
Jake Fried <samouri@users.noreply.github.com>
2022-02-11 15:13:33 -0500

    Add support for nextElementSibling and previousElementSibling (#1132)
    * Add support for nextElementSibling and previousElementSibling
    
    * update compat table
    
    * fix tests
    
    * also add to CharacterData
    
    * bump filesize
    
    * Flesh out one more aspect of test case.
    
    * bumped wrong filesize
    
    * babel loose mode compat: dont spread Set (#1133)
    
    * add comment node to test

--- src/test/node/nextSibling.test.ts
+++ src/test/node/nextSibling.test.ts
@@ -1,11 +1,16 @@
 import anyTest, { TestInterface } from 'ava';
+import { Comment } from '../../worker-thread/dom/Comment';
 import { Element } from '../../worker-thread/dom/Element';
+import { Text } from '../../worker-thread/dom/Text';
 import { createTestingDocument } from '../DocumentCreation';
 
 const test = anyTest as TestInterface<{
   node: Element;
   child: Element;
   childTwo: Element;
+  textNode: Text;
+  textNodeTwo: Text;
+  commentNode: Comment;
 }>;
 
 test.beforeEach((t) => {
@@ -15,6 +20,9 @@ test.beforeEach((t) => {
     node: document.createElement('div'),
     child: document.createElement('div'),
     childTwo: document.createElement('div'),
+    textNode: document.createTextNode('Hello'),
+    textNodeTwo: document.createTextNode('World'),
+    commentNode: document.createComment('comment'),
   };
 });
 
@@ -24,12 +32,14 @@ test('when a parent contains two children, the next sibling of the first is the 
   node.appendChild(child);
   node.appendChild(childTwo);
   t.deepEqual(child.nextSibling, childTwo);
+  t.deepEqual(child.nextElementSibling, childTwo);
 });
 
 test('when a node does not have a parent, its next sibling is null', (t) => {
   const { node } = t.context;
 
   t.is(node.nextSibling, null);
+  t.is(node.nextElementSibling, null);
 });
 
 test('when a node is the last child of a parent, the next sibling is null', (t) => {
@@ -38,4 +48,21 @@ test('when a node is the last child of a parent, the next sibling is null', (t) 
   node.appendChild(child);
   node.appendChild(childTwo);
   t.is(childTwo.nextSibling, null);
+  t.is(childTwo.nextElementSibling, null);
+});
+
+test('nextElementSibling skips over non-element nodes', (t) => {
+  const { node, child, childTwo, textNode, textNodeTwo, commentNode } = t.context;
+
+  node.appendChild(child);
+  node.appendChild(commentNode);
+  node.appendChild(textNode);
+  node.appendChild(childTwo);
+  node.appendChild(textNodeTwo);
+
+  t.is(child.nextElementSibling, childTwo);
+  t.is(commentNode.nextElementSibling, childTwo);
+  t.is(textNode.nextElementSibling, childTwo);
+  t.is(childTwo.nextElementSibling, null);
+  t.is(textNodeTwo.nextElementSibling, null);
 });
--- src/test/node/previousSibling.test.ts
+++ src/test/node/previousSibling.test.ts
@@ -1,11 +1,16 @@
 import anyTest, { TestInterface } from 'ava';
+import { Comment } from '../../worker-thread/dom/Comment';
 import { Element } from '../../worker-thread/dom/Element';
+import { Text } from '../../worker-thread/dom/Text';
 import { createTestingDocument } from '../DocumentCreation';
 
 const test = anyTest as TestInterface<{
   node: Element;
   child: Element;
   childTwo: Element;
+  textNode: Text;
+  textNodeTwo: Text;
+  commentNode: Comment;
 }>;
 
 test.beforeEach((t) => {
@@ -15,6 +20,9 @@ test.beforeEach((t) => {
     node: document.createElement('div'),
     child: document.createElement('div'),
     childTwo: document.createElement('div'),
+    textNode: document.createTextNode('Hello world'),
+    textNodeTwo: document.createTextNode('World'),
+    commentNode: document.createComment('comment'),
   };
 });
 
@@ -24,12 +32,14 @@ test('when a parent contains two children, the previous sibling of the second is
   node.appendChild(child);
   node.appendChild(childTwo);
   t.deepEqual(childTwo.previousSibling, child);
+  t.deepEqual(childTwo.previousElementSibling, child);
 });
 
 test('when a node does not have a parent, its previous sibling is null', (t) => {
   const { node } = t.context;
 
   t.is(node.previousSibling, null);
+  t.is(node.previousElementSibling, null);
 });
 
 test('when a node is the first child of a parent, the previous sibling is null', (t) => {
@@ -37,4 +47,24 @@ test('when a node is the first child of a parent, the previous sibling is null',
 
   node.appendChild(child);
   t.is(child.previousSibling, null);
+});
+
+test('previousElementSibling skips over text nodes', (t) => {
+  const { node, child, childTwo, textNode, textNodeTwo, commentNode } = t.context;
+
+  node.appendChild(child);
+  node.appendChild(commentNode);
+  node.appendChild(textNode);
+  node.appendChild(textNodeTwo);
+  node.appendChild(childTwo);
+
+  t.is(commentNode.previousElementSibling, child);
+  t.is(textNode.previousElementSibling, child);
+  t.is(textNodeTwo.previousElementSibling, child);
+  t.is(childTwo.previousElementSibling, child);
+
+  node.innerHTML = '';
+  node.appendChild(textNode);
+  node.appendChild(textNodeTwo);
+  t.is(textNodeTwo.previousElementSibling, null);
 });
